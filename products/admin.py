5ee4fd565deb51ced50a2f58e95710dbcc80f8b0
poojabhatta <bhattapuju3@gmail.com>
2019-07-03 12:17:33 +0545

    ..

---
+++ products/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from .models import Product, ProductFile, Category
+
+
+class ProductFileInline(admin.TabularInline):
+    model = ProductFile
+    extra = 1
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'slug', 'is_digital']
+    inlines = [ProductFileInline]
+    class Meta:
+        model = Product
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category)
